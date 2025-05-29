f50ee9dd238f3b02fc922d0b98f87be7c05753eb
samir-hussein <samirhussein274@gmail.com>
2025-05-29 18:47:19 +0300

    fix

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}" />
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
-    <meta name="apple-mobile-web-app-title" content="Happniess Perfume" />
+    <meta name="apple-mobile-web-app-title" content='Happniess Perfume | عطور السعادة' />
     <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
     <meta name="author" content="Samir Hussein">
     <meta name="robots" content="index, follow">
