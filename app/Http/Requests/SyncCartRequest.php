d83c04c5061d40131422e68516828ae7bb2cd367
samir-hussein <samirhussein274@gmail.com>
2025-06-01 23:28:33 +0300

    fix security issues

--- app/Http/Requests/SyncCartRequest.php
+++ app/Http/Requests/SyncCartRequest.php
@@ -3,46 +3,45 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Exception;
 
 class SyncCartRequest extends FormRequest
 {
-	/**
-	 * Determine if the user is authorized to make this request.
-	 */
-	public function authorize(): bool
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-	 */
-	public function rules(): array
-	{
-		if (count($this->products ?? []) > 100) {
-			throw new \Illuminate\Validation\ValidationException([
-				"products" => "You can't add more than 100 products to the cart",
-			]);
-		}
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        if (count($this->products ?? []) > 100) {
+            throw new Exception("You can't add more than 100 products to the cart");
+        }
 
-		return [
-			"products" => "required|array|max:100",
-			"products.*.product_id" => "required|exists:products,id",
-			"products.*.size" => "required|exists:product_sizes,size",
-			"products.*.quantity" => "required|numeric|min:1|max:100",
-		];
-	}
+        return [
+            "products" => "required|array|max:100",
+            "products.*.product_id" => "required|exists:products,id",
+            "products.*.size" => "required|exists:product_sizes,size",
+            "products.*.quantity" => "required|numeric|min:1|max:100",
+        ];
+    }
 
-	public function messages(): array
-	{
-		return [
-			"products.max" => "You can't add more than 100 products to the cart",
-			"products.*.product_id.exists" => "Product not found",
-			"products.*.size.exists" => "Size not found",
-			"products.*.quantity.min" => "Quantity must be at least 1",
-			"products.*.quantity.max" => "Quantity must be at most 100",
-		];
-	}
+    public function messages(): array
+    {
+        return [
+            "products.max" => "You can't add more than 100 products to the cart",
+            "products.*.product_id.exists" => "Product not found",
+            "products.*.size.exists" => "Size not found",
+            "products.*.quantity.min" => "Quantity must be at least 1",
+            "products.*.quantity.max" => "Quantity must be at most 100",
+        ];
+    }
 }
