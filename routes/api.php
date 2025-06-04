0398304ab83510220628c6867ead723bfbfc6c2e
samir-hussein <samirhussein274@gmail.com>
2025-06-04 19:57:08 +0300

    payment

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post("/test/webhook_json", function (Request $request) {
-    Log::info($request->all());
+	Log::info("post");
+	Log::info($request->all());
+});
+
+Route::get("/test/webhook_json", function (Request $request) {
+	Log::info("get");
+	Log::info($request->all());
 });
